19a3215f33a5d45eb3e93376813f47386a3225f0
Leonard <zilong.zheng@smail.nju.edu.cn>
2026-01-19 18:43:03 +0800

    Update checkin.py
    change space to cloud

--- checkin.py
+++ checkin.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def checkin(user_label,  cookie):
-    url = "https://glados.space/api/user/checkin"
+    url = "https://glados.cloud/api/user/checkin"
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Content-Type": "application/json;charset=UTF-8",
